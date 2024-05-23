f11ada442b0767d856f02e179e176a824a4fa813
mehulmb1 <65764700+mehulmb1@users.noreply.github.com>
2024-05-23 14:06:47 +0530

    testing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
       <ProductCard />
       <div className="flex justify-center -mt-10 mb-4">
         <Link to={'/allproducts'}>
-          <button className=' bg-gray-300 px-5 py-2 rounded-xl'>See more</button>
+          <button className=' bg-gray-300 px-5 py-2 rounded-xl'>See More Products</button>
         </Link>
       </div>
       <Track />
